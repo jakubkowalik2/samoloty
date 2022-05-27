ba69ed101d57cc4a13852c461b5c48a50df79998
jakubkowalik2 <kkowalikstudia@gmail.com>
2022-05-27 17:58:05 +0200

    start destinaton

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { DateAndWeatherComponent } from './components/date-and-weather/date-and-weather.component';
 import { HttpClientModule } from '@angular/common/http';
 import { StartDestinationComponent } from './components/start-destination/start-destination.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -17,6 +18,7 @@ import { StartDestinationComponent } from './components/start-destination/start-
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/start-destination/start-destination.component.ts
+++ src/app/components/start-destination/start-destination.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgbDropdown } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-start-destination',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-destination.component.scss']
 })
 export class StartDestinationComponent implements OnInit {
+  // @ViewChild(NgbDropdown, { static: true })
+  // public dropdown: NgbDropdown;
+
+  selectedStart = 'Start'
+  selectedDestination = 'Destination'
+
+  startCities = ["Katowice", "Piekary Śląskie"];
+  destinationCities = ["Londyn", "Nowy Yo", "Kingstone"];
 
   constructor() { }
 
   ngOnInit(): void {
+    
   }
+
+  onSelectedStartCity(city: string) : void {
+    console.log(city)
+    this.selectedStart = city;
+  }
+
+  onSelectedDestinationCity(city: string) : void {
+    console.log(city)
+    this.selectedDestination = city;
+  }
+
 
 }
